7b26c2a90a9b3218ad2cfcfe671538422b748fbf
Cunqi <cx363@nyu.edu>
2014-10-29 15:53:20 -0400

    use core data to store section info

--- iDouBan/iDouBan/MusicSectionController.swift
+++ iDouBan/iDouBan/MusicSectionController.swift
@@ -1,5 +1,5 @@
 //
-//  MusicSectionController.swift
+//  sectionsController.swift
 //  iDouBan
 //
 //  Created by Cunqi.X on 10/13/14.
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class MusicSectionController: UITableViewController,HttpProtocol {
 	
@@ -15,8 +16,10 @@ class MusicSectionController: UITableViewController,HttpProtocol {
 	Constant
 	=================================================================================
 	*/
-    let musicSectionURL = "http://www.douban.com/j/app/radio/channels"
+	let ENTITY_NAME = "Section"
+    let sectionsURL = "http://www.douban.com/j/app/radio/channels"
 	let httpController = HttpController()
+	let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
 	
 	/*
 	=================================================================================
@@ -24,9 +27,9 @@ class MusicSectionController: UITableViewController,HttpProtocol {
 	=================================================================================
 	*/
 	
-    var musicInSection = "http://www.douban.com/j/app/radio/people?app_name=radio_desktop_win&version=100&user_id=&expire=&token=&sid=&h=&channel="
+    var musicsInSection = "http://www.douban.com/j/app/radio/people?app_name=radio_desktop_win&version=100&user_id=&expire=&token=&sid=&h=&channel="
     
-    var musicSection: NSArray = NSArray()
+    var sections: NSArray = [NSManagedObject]()
     var delegate : SongLoadDelegate?
 
 	/*
@@ -36,15 +39,26 @@ class MusicSectionController: UITableViewController,HttpProtocol {
 	*/
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //load data from Internet.
-        httpController.delegate = self
-        httpController.onSearch(musicSectionURL)
+		self.httpController.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+	
+	override func viewWillAppear(animated: Bool) {
+		let managedContext = self.appDelegate.managedObjectContext!
+		let fetchRequest = NSFetchRequest(entityName: ENTITY_NAME)
+		
+		let fetchResult: NSArray = managedContext.executeFetchRequest(fetchRequest, error: nil)!
+		
+		if fetchResult.count > 0 {
+			self.sections = fetchResult
+		}else {
+			//load data from Internet.
+			self.httpController.onSearch(sectionsURL)
+		}
+	}
 	
 	/*
 	=================================================================================
@@ -52,24 +66,24 @@ class MusicSectionController: UITableViewController,HttpProtocol {
 	=================================================================================
 	*/
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.musicSection.count
+        return self.sections.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "section")
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: ENTITY_NAME)
         
-        let section: NSDictionary = musicSection[indexPath.row] as NSDictionary
-        
-        cell.textLabel.text = (section["name"] as String)
+        let section: AnyObject = sections[indexPath.row]
+        cell.textLabel.text = section.valueForKey("name") as String?
         
         return cell
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
+		
+		let section: AnyObject = sections[indexPath.row]
         //load selected data
-		let row: Character = Character(UnicodeScalar(indexPath.row))
-		var resultURL = musicInSection + "\(indexPath.row)&type=n"
+		let row = section.valueForKey("channel_id") as String
+		var resultURL = musicsInSection + "\(row)&type=n"
 		
 		//update playList
 		delegate?.didLoad(resultURL)
@@ -79,17 +93,60 @@ class MusicSectionController: UITableViewController,HttpProtocol {
 	
 	/*
 	=================================================================================
-	The Implementation of Private Method.
+	The Implementation of Protocol
 	=================================================================================
 	*/
     func didReceiveResults(result: NSDictionary) {
         if (result["channels"] != nil) {
-            self.musicSection = result["channels"]! as NSArray
-
+            let data = result["channels"]! as NSArray
+			
+			sections = saveData(data)
+			
             self.tableView.reloadData()
             
         }
 
     }
-    
+	
+	/*
+	=================================================================================
+	The Implementation of Private Method
+	=================================================================================
+	*/
+	
+	private func saveData(data: NSArray) -> NSArray{
+		let managedContext = self.appDelegate.managedObjectContext!
+		
+		return batchSaveData(data, managedContext: managedContext)
+	
+	}
+	
+	private func batchSaveData(data: NSArray, managedContext: NSManagedObjectContext) -> NSArray {
+		
+		let formatter: NSNumberFormatter = NSNumberFormatter()
+		var result:[NSManagedObject] = []
+		
+		var channel_id: NSNumber = 0
+		for map in data {
+			
+			let section = NSEntityDescription.insertNewObjectForEntityForName(ENTITY_NAME, inManagedObjectContext: managedContext) as NSManagedObject
+			
+			if(map.objectForKey("channel_id")!.isKindOfClass(NSNumber)) {
+				channel_id = map["channel_id"] as NSNumber
+			}else if(map.objectForKey("channel_id")!.isKindOfClass(NSString)){
+				channel_id = formatter.numberFromString(map["channel_id"] as NSString)!
+			}
+			
+			let name = map["name"]
+			
+			section.setValue(name, forKey: "name")
+			section.setValue(channel_id, forKey: "channel_id")
+			
+			managedContext.save(nil)
+			
+			result.append(section)
+		}
+		
+		return result
+	}
 }
